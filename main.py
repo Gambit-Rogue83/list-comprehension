f986a2d801a51dde0d7867621de7f8fe771c9adc
Gambit-Rogue83 <141782770+Gambit-Rogue83@users.noreply.github.com>
2024-04-30 20:40:23 +0000

    more dict comprehension

--- main.py
+++ main.py
@@ -21,4 +21,22 @@ names = ["Alex", "Beth", "Dave", "Elizabeth", "Frank", "Jeff", "Kenneth", "Liam"
 
 student_scores = {student:random.randint(45, 100) for student in names}
 
-print(student_scores)
+
+passing_score = {student:score for (student, score) in student_scores.items() if score >= 60 }
+
+# print(passing_score)
+
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+
+result = {word:len(word) for word in sentence.split()}
+print(result)
+
+weather_c = {
+  "Monday": 12, "Tuesday": 14, "Wednesday": 15,
+  "Thursday": 14, "Friday": 21, "Saturday": 22,
+  "Sunday": 24
+}
+
+weather_f = {day:temp * 9/5 + 32 for (day, temp) in weather_c.items()}
+
+print(weather_f)

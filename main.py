c4c4211a60442498f1417b40eef9de90f32badde
Gambit-Rogue83 <141782770+Gambit-Rogue83@users.noreply.github.com>
2024-04-30 17:22:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# numbers = [1, 2, 3, 4, 5]
+# new_numbers = [num + 1 for num in numbers]
+# print(new_numbers)
+
+# string = "Shak"
+# new_string = [letter for letter in string]
+# print(new_string)
+
+# double_list = [num * 2 for num in range(1, 5)]
+# print(double_list)
+
+names = ["Alex", "Beth", "Dave", "Elizabeth", "Frank", "Jeff", "Kenneth", "Liam"]
+
+short_names = [name for name in names if len(name) == 4]
+
+cap_names = [name.upper() for name in names if len(name) > 4]
+
+print(cap_names)

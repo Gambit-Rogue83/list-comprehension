bdc27c7e21aa0d4360f26d67c3e40d1bcc68ca99
Gambit-Rogue83 <141782770+Gambit-Rogue83@users.noreply.github.com>
2024-04-30 19:24:11 +0000

    dictionary comprehension

--- main.py
+++ main.py
@@ -9,10 +9,16 @@
 # double_list = [num * 2 for num in range(1, 5)]
 # print(double_list)
 
+import random
+
 names = ["Alex", "Beth", "Dave", "Elizabeth", "Frank", "Jeff", "Kenneth", "Liam"]
 
-short_names = [name for name in names if len(name) == 4]
+# short_names = [name for name in names if len(name) == 4]
 
-cap_names = [name.upper() for name in names if len(name) > 4]
+# cap_names = [name.upper() for name in names if len(name) > 4]
 
-print(cap_names)
+# print(cap_names)
+
+student_scores = {student:random.randint(45, 100) for student in names}
+
+print(student_scores)
